5558a6cd0590256952e1698d34dcaf86f7804518
Anton Jenkins <anton@pixellatedvisions.com>
2009-06-29 18:55:32 +0100

    initial import

---
+++ Rakefile
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'rake'
+require 'echoe'
+
+Echoe.new('model_sync', '0.1.0') do |p|
+  p.description    = "Sync changes to an ActiveRecord model to another model"
+  p.url            = "http://github.com/antonjenkins/model_sync"
+  p.author         = "Anton Jenkins"
+  p.email          = "info@pixellatedvisions.com"
+  p.ignore_pattern = ["tmp/*", "script/*"]
+  p.development_dependencies = []
+end
+
+Dir["#{File.dirname(__FILE__)}/tasks/*.rake"].sort.each { |ext| load ext }
---
+++ lib/model_sync.rb
@@ -0,0 +1,47 @@
+module ModelSync
+  module Base
+    def self.included(klass)
+      klass.class_eval do
+        extend Config
+      end
+    end
+
+    module Config
+      attr_reader :slave_model_name, :slave_model_class, :relationship, :mappings
+
+      def model_sync(options)
+        @slave_model_name = options[:sync_to].to_s.downcase
+        @slave_model_class = Kernel.const_get(@slave_model_name.classify)
+        @relationship = options[:relationship]
+        @mappings = options[:mappings]
+
+        # Add a callback to sync_changes on every save
+        self.after_save :sync_changes
+      end
+    end
+
+    def sync_changes
+      # If we can find a slave instance...
+      if slave_instance = find_slave_instance
+        # ... then update all the attributes which we've mapped
+        self.class.mappings.each do |source, dest|
+          slave_instance.update_attribute(dest, self.read_attribute(source))
+        end
+      end
+    end
+
+private
+    def find_slave_instance
+      self.class.slave_model_class.find(:first, 
+                                        :conditions => "#{self.class.relationship.values.first.to_s} = #{self.read_attribute(self.class.relationship.keys.first)}")
+    end
+  end
+end
+
+if defined?(::ActiveRecord)
+  module ::ActiveRecord
+    class Base
+      include ModelSync::Base
+    end
+  end
+end
